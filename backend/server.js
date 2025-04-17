2100863e567bb9aec128ed9c26801f3c8cb11f08
Brimesh Desai <52075754+brimesh123@users.noreply.github.com>
2025-04-17 12:19:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: true, // Allow all origins, will be restricted by Render's security
+    origin: 'https://clone-hostel.vercel.app', // Allow all origins, will be restricted by Render's security
     credentials: true, // Allow cookies to be sent/received
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS' , 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
